32e22dbfaca79645d72ec0e64ef6890e2de69ec6
november1997pliyi-netizen <november.1997.pliyi@gmail.com>
2026-04-27 18:09:42 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDeZBNevZdOArbLpY8o9J6wPpMGNo1KyJM",
   authDomain: "let-s-fall-in-love-72dbd.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
   appId: "1:164113630325:web:22c2ed768d3fc85513e43c"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
